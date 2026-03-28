ebf3a1e14e2d72c6146acf369e960750f5114de8
Bukaslye <oguinyefavour@yahoo.com>
2026-03-28 15:05:55 +0000

    Step 6: All containers running successfully

--- services/service-a/app.py
+++ services/service-a/app.py
@@ -1,10 +1,10 @@
-from flask import flask, jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-return jsonify({
+    return jsonify({
         "service": "service-a",
         "status": "running",
         "message": "Welcome to Service A - Authorised access only!"
